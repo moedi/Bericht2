86dd100e65d924a343792d341db23d1d2c6b487f
Philipp <Philipp@192.168.1.102>
2012-12-08 22:59:08 +0100

    projekt2

---
+++ bericht/src/print/Ausgabe.java
@@ -0,0 +1,21 @@
+package print;
+
+public class Ausgabe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Tescht");
+
+System.out.println("Tescht");
+System.out.println("Tescht");
+
+System.out.println("Tescht");
+
+System.out.println("Tescht");
+
+	}
+
+}
