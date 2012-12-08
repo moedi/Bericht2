401d3cab3a7d92bf03bf5f7c53dc1995350daaf8
Philipp <Philipp@192.168.1.102>
2012-12-08 22:35:36 +0100

    neu

---
+++ Bericht2/src/berichtv3/Ausgabe.java
@@ -0,0 +1,27 @@
+package berichtv3;
+
+public class Ausgabe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("****************");
+		System.out.println("55555555");
+		System.out.println("4444444");
+		System.out.println("333333");
+		System.out.println("22222");
+		System.out.println("1111");
+		System.out.println("000");
+		
+		System.out.println("000");
+		
+		
+		
+		
+		
+	}
+
+}
